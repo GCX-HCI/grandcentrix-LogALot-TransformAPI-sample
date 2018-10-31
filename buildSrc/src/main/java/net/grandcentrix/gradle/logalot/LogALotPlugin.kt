d6b7ddddb930457fd0750b7e3ffa03b2eb6356c4
Bjoern Quentin <bjoern.quentin@grandcentrix.net>
2018-10-31 13:17:13 +0100

    runtime now needs to be added manually

--- buildSrc/src/main/java/net/grandcentrix/gradle/logalot/LogALotPlugin.kt
+++ buildSrc/src/main/java/net/grandcentrix/gradle/logalot/LogALotPlugin.kt
@@ -24,24 +24,6 @@ open class LogALotPlugin : Plugin<Project> {
         @Suppress("UnsafeCast")
         val android = target.extensions.findByName("android") as BaseExtension
         android.registerTransform(LogALotTransformer(android, extension, target.logger))
-
-        target.afterEvaluate {
-            val shouldApplyTransform = gradle.startParameter.taskRequests.find { taskExecutionRequest ->
-                taskExecutionRequest.args.find { arg ->
-                    extension.applyFor?.find {
-                        arg.endsWith(
-                            it,
-                            true
-                        )
-                    } != null
-                } != null
-            } != null
-
-            // don't add dependency to runtime if we are not going to use it
-            if (shouldApplyTransform) {
-                target.dependencies.add("implementation", "net.grandcentrix.gradle.logalot:runtime:0.0.1")
-            }
-        }
     }
 
 }
